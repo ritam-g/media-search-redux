c6e0a010bceb13fa6ef26806e2ca3df18701a2c1
ritam-g <ritammaty2006@gmail.com>
2026-01-17 15:30:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+     <main className='w-screen h-screen bg-black text-white'>
+      <h1>I am Ritam</h1>
+      </main> 
+    </>
+  )
+}
+
+export default App
