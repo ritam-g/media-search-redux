450078526f20c6d90c36aa4670ed7b1c06cc63a7
ritam-g <ritammaty2006@gmail.com>
2026-01-22 18:33:39 +0530

    redux lerning done

--- src/redux/features/collectionSlice.js
+++ src/redux/features/collectionSlice.js
@@ -1,1 +1,25 @@
-    
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+    items: JSON.parse(localStorage.getItem('collection')) || []
+}
+export const collectionSlice = createSlice({
+    name: "colleciton",
+    initialState,
+    reducers: {
+        addcollection: (state, action) => {
+            let exist = state.items.find(item => item.id == action.payload.id)
+            if (!exist) {
+                state.items.push(action.payload)
+                localStorage.setItem('collection', JSON.stringify(state.items))
+
+            }
+        },
+        clearCollection: (state, action) => {
+            state.items=[]
+            localStorage.removeItem('collection')
+
+        },
+    }
+})
+export const { addcollection, clearCollection } = collectionSlice.actions
+export default collectionSlice.reducer
